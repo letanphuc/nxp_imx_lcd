9c76a83f9a49aa53f1b3ebddad8e8fc286a49d81
Phuc Le <tanphuc.le@gmail.com>
2022-07-13 11:50:51 +0700

    add lcd example from nxp

---
+++ utilities/utility_debug_console_MIMXRT595S_cm33.cmake
@@ -0,0 +1,18 @@
+include_guard(GLOBAL)
+message("utility_debug_console component is included.")
+
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+${CMAKE_CURRENT_LIST_DIR}/fsl_str.c
+${CMAKE_CURRENT_LIST_DIR}/fsl_debug_console.c
+)
+
+
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PRIVATE
+${CMAKE_CURRENT_LIST_DIR}/
+)
+
+
+include(component_serial_manager_MIMXRT595S_cm33)
+
+include(driver_common_MIMXRT595S_cm33)
+
